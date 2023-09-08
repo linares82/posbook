5c81530933c06e7fc02ef0a792e6241e0a0f941a
linares82 <linares82@gmail.com>
2023-09-07 18:04:48 -0600

    reportes de corte con timepicker con formato

--- resources/js/Pages/Movements/reportes/RptCortePlantel.vue
+++ resources/js/Pages/Movements/reportes/RptCortePlantel.vue
@@ -21,7 +21,7 @@
         <a-col :md="1"></a-col>
         <a-col :md="7">
             <a-form-item compact label="Fecha de:" name="fecha_f" :rules="[{ required: true, message: 'Por favor captura la información solicitada!' }]">
-                <a-date-picker show-time v-model:value="formCashBox.fecha_f" format="YYYY-MM-DD" :bordered="true" />
+                <a-date-picker show-time v-model:value="formCashBox.fecha_f" format="YYYY-MM-DD HH:mm:ss" :bordered="true" />
                 <div v-if="errors.fecha_f">
                     <div role="alert" class="ant-form-item-explain-error" style="" v-text="errors.fecha_f"></div>
                 </div>
@@ -30,7 +30,7 @@
         <a-col :md="1"></a-col>
         <a-col :md="7">
             <a-form-item compact label="Fecha a:" name="fecha_t" :rules="[{ required: true, message: 'Por favor captura la información solicitada!' }]">
-                <a-date-picker show-time v-model:value="formCashBox.fecha_t" format="YYYY-MM-DD" :bordered="true" />
+                <a-date-picker show-time v-model:value="formCashBox.fecha_t" format="YYYY-MM-DD HH:mm:ss" :bordered="true" />
                 <div v-if="errors.fecha_t">
                     <div role="alert" class="ant-form-item-explain-error" style="" v-text="errors.fecha_t"></div>
                 </div>
@@ -69,6 +69,7 @@ import {
     Inertia
 } from "@inertiajs/inertia";
 import axios from 'axios';
+
 //import locale from 'ant-design-vue/es/date-picker/locale/es_Es';
 /*
 import dayjs from 'dayjs';
